8cb005fc7fe67b06582da4d251ce8b899bf8416b
Lukas Scholz <40420097+therealgherkhin@users.noreply.github.com>
2021-05-12 09:27:13 +0200

    Delete secrets.java

--- src/main/java/com/company/CSAServer/secrets.java
+++
@@ -1,7 +0,0 @@
-package com.company.CSAServer;
-
-public interface secrets {
-    java.lang.String url = "jdbc:mysql://lukasscholz.de:3306/d035dbd4?useSSL=false&serverTimezone=Europe/Warsaw";
-    java.lang.String user = "d035dbd4";
-    java.lang.String password = "uN5s45ppG2G3YwJ5";
-}
